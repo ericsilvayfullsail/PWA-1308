4603377c04d5f150b0166366cd84736cc7668e96
Eric Silvay <esilvay@fullsail.com>
2013-08-24 11:28:02 -0400

    Push Day 8

---
+++ day 8/goal8_custom_library/js/main.js
@@ -0,0 +1,22 @@
+/* 
+	Arena Battle!!!
+	Author: JavaScript Development Team
+	PWA1: Goal 8
+*/
+
+/*
+	=====================================================
+	Javascript pattern: self-executing function
+	syntax: (function(){})();
+	
+	Create a private scope for the application's main code
+*/
+
+(function(){
+	
+
+
+})();
+
+
+
---
+++ day 8/goal8_custom_library/js/wizard.js
@@ -0,0 +1,8 @@
+// Wizard Class File
+// Keep the constructor small- Use the prototype
+
+
+
+
+
+
